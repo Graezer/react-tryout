0563e67ad95950de26f349007e76aa1284901f62
geza steinert <gwasteinert@gmail.com>
2022-06-23 23:38:16 +0200

    tryout implementing bootstrap

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <p>Edit <code>src/App.js</code> and save to reload.</p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
-        </header>
 
-        <div className="todo">
-          < TodoList todos={todos} />
-          <input type="text" />
-          <button>+</button>
-          <button>clear</button>
-        </div>
+          <div className="todo">
+            < TodoList todos={todos} />
+            <input type="text" />
+            <button className="btn btn-primary">+</button>
+            <button className="btn btn-warning">clear</button>
+          </div>
+        </header>
       </div>
     </>
   );
